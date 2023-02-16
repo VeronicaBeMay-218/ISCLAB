b4416b88daee7d38e2d8fd9e64593111cc342b3c
VeronicaBeMay-218 <veronica.19070007@itsmotul.edu.mx>
2023-02-16 01:22:27 -0600

    paso elementos

--- src/components/lista.vue
+++ src/components/lista.vue
@@ -1,86 +1,53 @@
+<script>
+import data from './../assets/datos.json'
+import Elementoli from '../components/Elementoli.vue'
+
+export default{
+    data(){
+        return{
+            lista:data
+        }
+        
+    },
+    components: {
+            Elementoli
+        }
+}
+</script>
+
+
 <template>
-  <div class="item">
-    <i>
-      <slot name="icon"></slot>
-    </i>
-    <div class="details">
-      <h3>
-        <slot name="heading"></slot>
-      </h3>
-      <slot></slot>
-    </div>
+    <div class="">
+ 
+  <div class="px-6 py-4">
+
+    <section class="w-120 rounded overflow-hidden shadow-lg p-12 m-10 snap-start bg-teal-400" v-for="etapa in lista.etapas">
+    <ul>
+        <ul class="font-semibold font-titular text-3xl">Sección {{etapa.id}}. {{etapa.nombre}} </ul> 
+        
+        
+    </ul>
+
+    <ul class="px-5 py-3 py-3 rounded-md">
+         <Elementoli v-for="paso in etapa.pasos" :paso="paso"></Elementoli>
+         
+    </ul>
+
+
+</section>
+
+
+
+
+    <p class="text-gray-700 text-base">
+    
+    </p>
   </div>
+  
+</div>
+    
+   
+
 </template>
 
-<style scoped>
-.item {
-  margin-top: 2rem;
-  display: flex;
-}
 
-.details {
-  flex: 1;
-  margin-left: 1rem;
-}
-
-i {
-  display: flex;
-  place-items: center;
-  place-content: center;
-  width: 32px;
-  height: 32px;
-
-  color: var(--color-text);
-}
-
-h3 {
-  font-size: 1.2rem;
-  font-weight: 500;
-  margin-bottom: 0.4rem;
-  color: var(--color-heading);
-}
-
-@media (min-width: 1024px) {
-  .item {
-    margin-top: 0;
-    padding: 0.4rem 0 1rem calc(var(--section-gap) / 2);
-  }
-
-  i {
-    top: calc(50% - 25px);
-    left: -26px;
-    position: absolute;
-    border: 1px solid var(--color-border);
-    background: var(--color-background);
-    border-radius: 8px;
-    width: 50px;
-    height: 50px;
-  }
-
-  .item:before {
-    content: ' ';
-    border-left: 1px solid var(--color-border);
-    position: absolute;
-    left: 0;
-    bottom: calc(50% + 25px);
-    height: calc(50% - 25px);
-  }
-
-  .item:after {
-    content: ' ';
-    border-left: 1px solid var(--color-border);
-    position: absolute;
-    left: 0;
-    top: calc(50% + 25px);
-    height: calc(50% - 25px);
-  }
-
-  .item:first-of-type:before {
-    display: none;
-  }
-
-  .item:last-of-type:after {
-    display: none;
-  }
-}
-</style>
